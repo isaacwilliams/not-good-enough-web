85a580abfed042205a6c55bee98aa5d99715738a
Isaac Williams <isaac@isaacwilliams.net>
2020-01-14 13:50:46 +1100

    Add gh-pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
     siteMetadata: {
-        title: `Gatsby Starter Blog`,
-        author: `Kyle Mathews`,
-        description: `A starter blog demonstrating what Gatsby can do.`,
-        siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+        title: `Not Good Enough`,
+        author: `Not Good Enough`,
+        description: `A podcast about the climate crisis.`,
+        siteUrl: 'https://notgoodpod.com',
         social: {
-            twitter: `kylemathews`,
+            twitter: `notgoodpod`,
         },
     },
     plugins: [
